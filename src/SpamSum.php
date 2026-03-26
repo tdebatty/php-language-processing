c4a796f4517022ec5d867c0d955c1c85bdff85b1
Thibault Debatty <thibault.debatty@proton.me>
2026-03-26 14:46:33 +0100

    fix phpcs

--- src/SpamSum.php
+++ src/SpamSum.php
@@ -7,129 +7,129 @@ namespace webd\language;
  */
 class SpamSum
 {
-  /**
-   * Compute the SpamSum of string using default parameters:
-   * length = 64 characters
-   * 64 possible letters (Base64)
-   * min blocksize = 3
-   * block size computed automatically
-   *
-   * @param string $string
-   * @return \webd\language\SpamSum
-   */
-    public static function Hash($string)
+    /**
+     * Compute the SpamSum of string using default parameters:
+     * length = 64 characters
+     * 64 possible letters (Base64)
+     * min blocksize = 3
+     * block size computed automatically
+     *
+     * @param string $string
+     * @return \webd\language\SpamSum
+     */
+    public static function hash($string)
     {
         $ss = new SpamSum();
-        $ss->HashString($string);
+        $ss->hashString($string);
         return $ss;
     }
 
     const HASH_PRIME = 0x01000193;
     const HASH_INIT = 0x28021967;
     const B64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  
+
     protected $SPAMSUM_LENGTH = 64;
     protected $LETTERS = 64;
     protected $BLOCKSIZE = 0;
     protected $MIN_BLOCKSIZE = 3;
     protected $auto_blocksize = true;
-  
+
     protected $left;
     protected $right;
-  
-  /**
-   * Set a different hash length
-   * min = 1, default = 64
-   *
-   * @param int $l
-   */
-    public function SetHashLength($l)
+
+    /**
+     * Set a different hash length
+     * min = 1, default = 64
+     *
+     * @param int $l
+     */
+    public function setHashLength($l)
     {
         $this->SPAMSUM_LENGTH = $l;
     }
-  
-  /**
-   * Set the number of letters to use to create the hash
-   * min = 2, max = 64, default = 64 (base64)
-   * @param int $l
-   */
-    public function SetLetters($l)
+
+    /**
+     * Set the number of letters to use to create the hash
+     * min = 2, max = 64, default = 64 (base64)
+     * @param int $l
+     */
+    public function setLetters($l)
     {
         $this->LETTERS = $l;
     }
-  
-  /**
-   * Manually set the minimum block size
-   * min = 1, default = 3
-   * @param int $s
-   */
-    public function SetMinBlocksize($s)
+
+    /**
+     * Manually set the minimum block size
+     * min = 1, default = 3
+     * @param int $s
+     */
+    public function setMinBlocksize($s)
     {
         $this->MIN_BLOCKSIZE = $s;
     }
-  
-  /**
-   * Set the blok size manually, so that it won't be computed from the length of
-   * the string
-   * @param int $s
-   */
-    public function SetBlockSize($s)
+
+    /**
+     * Set the blok size manually, so that it won't be computed from the length of
+     * the string
+     * @param int $s
+     */
+    public function setBlockSize($s)
     {
         $this->BLOCKSIZE = $s;
         $this->auto_blocksize = false;
     }
-  
-  /**
-   *
-   * @param string $string
-   * @return \webd\language\SpamSum
-   */
-    public function HashString($string)
+
+    /**
+     *
+     * @param string $string
+     * @return \webd\language\SpamSum
+     */
+    public function hashString($string)
     {
         $b64 = self::B64;
         $length = strlen($string);
 
-        $in = unpack('C*', $string);
+        $in = unpack("C*", $string);
 
-      // Reindex (to start from 0)
+        // Reindex (to start from 0)
         foreach ($in as $k => $v) {
             $in[$k - 1] = $v;
         }
         unset($in[count($in)]);
 
-      // Guess a a reasonable block size
+        // Guess a a reasonable block size
         if ($this->auto_blocksize) {
             $this->BLOCKSIZE = $this->MIN_BLOCKSIZE;
-      
+
             while ($this->BLOCKSIZE * $this->SPAMSUM_LENGTH < $length) {
                 $this->BLOCKSIZE = $this->BLOCKSIZE * 2;
             }
         }
-    
+
         again:
 
-        $this->left = array();
-        $this->right = array();
+        $this->left = [];
+        $this->right = [];
 
         $k = $j = 0;
         $h3 = $h2 = self::HASH_INIT;
-        $h = $this->rolling_hash_reset();
+        $h = $this->rollingHashReset();
 
         for ($i = 0; $i < $length; $i++) {
-          /* at each character we update the rolling hash and the normal
-           * hash. When the rolling hash hits the reset value then we emit
-           * the normal hash as a element of the signature and reset both
-           * hashes
-           */
-            $h = $this->rolling_hash($in[$i]);
-            $h2 = self::sum_hash($in[$i], $h2);
-            $h3 = self::sum_hash($in[$i], $h3);
+            /* at each character we update the rolling hash and the normal
+             * hash. When the rolling hash hits the reset value then we emit
+             * the normal hash as a element of the signature and reset both
+             * hashes
+             */
+            $h = $this->rollingHash($in[$i]);
+            $h2 = self::sumHash($in[$i], $h2);
+            $h3 = self::sumHash($in[$i], $h3);
 
-            if ($h % $this->BLOCKSIZE == ($this->BLOCKSIZE - 1)) {
-              /* we have hit a reset point. We now emit a hash which is based
-               * on all chacaters in the piece of the string between the last
-               * reset point and this one
-               */
+            if ($h % $this->BLOCKSIZE == $this->BLOCKSIZE - 1) {
+                /* we have hit a reset point. We now emit a hash which is based
+                 * on all chacaters in the piece of the string between the last
+                 * reset point and this one
+                 */
                 $this->left[$j] = $b64[$h2 % $this->LETTERS];
                 if ($j < $this->SPAMSUM_LENGTH - 1) {
                     /* we can have a problem with the tail overflowing. The easiest way
@@ -142,11 +142,11 @@ class SpamSum
                 }
             }
 
-          /* this produces a second signature with a block size of block_size*2.
-           * By producing dual signatures in this way the effect of small changes
-           * in the string near a block size boundary is greatly reduced.
-           */
-            if ($h % ($this->BLOCKSIZE * 2) == (($this->BLOCKSIZE * 2) - 1)) {
+            /* this produces a second signature with a block size of block_size*2.
+             * By producing dual signatures in this way the effect of small changes
+             * in the string near a block size boundary is greatly reduced.
+             */
+            if ($h % ($this->BLOCKSIZE * 2) == $this->BLOCKSIZE * 2 - 1) {
                 $this->right[$k] = $b64[$h3 % $this->LETTERS];
                 if ($k < $this->SPAMSUM_LENGTH / 2 - 1) {
                     $h3 = self::HASH_INIT;
@@ -155,89 +155,89 @@ class SpamSum
             }
         }
 
-      /* If we have anything left then add it to the end. This ensures that the
-       * last part of the string is always considered
-       */
+        /* If we have anything left then add it to the end. This ensures that the
+         * last part of the string is always considered
+         */
         if ($h != 0) {
             $this->left[$j] = $b64[$h2 % $this->LETTERS];
             $this->right[$k] = $b64[$h3 % $this->LETTERS];
         }
 
-      /* Our blocksize guess may have been way off - repeat if necessary
-       */
-        if ($this->auto_blocksize
-            && $this->BLOCKSIZE > $this->MIN_BLOCKSIZE
-            && $j < $this->SPAMSUM_LENGTH / 2) {
+        /* Our blocksize guess may have been way off - repeat if necessary
+         */
+        if ($this->auto_blocksize &&
+            $this->BLOCKSIZE > $this->MIN_BLOCKSIZE &&
+            $j < $this->SPAMSUM_LENGTH / 2
+        ) {
             $this->BLOCKSIZE = $this->BLOCKSIZE / 2;
             goto again;
         }
 
         return $this;
     }
-  
+
     public function __toString()
     {
-        return
-        $this->BLOCKSIZE . ":" . $this->Left() . ":" . $this->Right();
+        return $this->BLOCKSIZE . ":" . $this->left() . ":" . $this->right();
     }
-  
-    public function BlockSize()
+
+    public function blockSize()
     {
         return $this->BLOCKSIZE;
     }
-  
-    public function Left()
+
+    public function left()
     {
         return implode("", $this->left);
     }
-  
-    public function Right()
+
+    public function right()
     {
         return implode("", $this->right);
     }
-  
-  /* A simple non-rolling hash, based on the FNV hash
-   */
-    protected static function sum_hash($c, $h)
+
+    /* A simple non-rolling hash, based on the FNV hash
+     */
+    protected static function sumHash($c, $h)
     {
         $h = ($h * self::HASH_PRIME) % pow(2, 32);
         $h = ($h ^ $c) % pow(2, 32);
         return $h;
     }
-  
 
-  /* A rolling hash, based on the Adler checksum. By using a rolling hash
-   * we can perform auto resynchronisation after inserts/deletes internally,
-   * h1 is the sum of the bytes in the window and h2 is the sum of the bytes
-   * times the index h3 is a shift/xor based rolling hash, and is mostly
-   * needed to ensure that we can cope with large blocksize values
-   */
+    /* A rolling hash, based on the Adler checksum. By using a rolling hash
+     * we can perform auto resynchronisation after inserts/deletes internally,
+     * h1 is the sum of the bytes in the window and h2 is the sum of the bytes
+     * times the index h3 is a shift/xor based rolling hash, and is mostly
+     * needed to ensure that we can cope with large blocksize values
+     */
     const ROLLING_WINDOW = 7;
-  
-    protected $rolling_window = array();
+
+    protected $rolling_window = [];
     protected $rolling_h1;
     protected $rolling_h2;
     protected $rolling_h3;
     protected $rolling_n;
 
-    protected function rolling_hash($c)
+    protected function rollingHash($c)
     {
         $this->rolling_h2 -= $this->rolling_h1;
         $this->rolling_h2 += self::ROLLING_WINDOW * $c;
 
         $this->rolling_h1 += $c;
-        $this->rolling_h1 -= $this->rolling_window[$this->rolling_n % self::ROLLING_WINDOW];
+        $this->rolling_h1 -=
+            $this->rolling_window[$this->rolling_n % self::ROLLING_WINDOW];
 
         $this->rolling_window[$this->rolling_n % self::ROLLING_WINDOW] = $c;
         $this->rolling_n++;
 
-        $this->rolling_h3 = ($this->rolling_h3 << 5) & 0xFFFFFFFF;
+        $this->rolling_h3 = ($this->rolling_h3 << 5) & 0xffffffff;
         $this->rolling_h3 ^= $c;
 
         return $this->rolling_h1 + $this->rolling_h2 + $this->rolling_h3;
     }
 
-    protected function rolling_hash_reset()
+    protected function rollingHashReset()
     {
         for ($i = 0; $i < self::ROLLING_WINDOW; $i++) {
             $this->rolling_window[$i] = 0;
--- tests/src/webd/language/SpamSumTest.php
+++ tests/src/webd/language/SpamSumTest.php
@@ -6,88 +6,73 @@ use PHPUnit\Framework\TestCase;
 
 class SpamSumTest extends TestCase
 {
-
     /**
      * @var SpamSum
      */
     protected $object;
-    
-    protected $str1 = "A string, for example the title of a spam, or the text of an email, or even the content of a webpage... Who knows? With some additional characters to make it long enough... and some more characters, I hope I will evnetually reach a sufficient lenght....\n";
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp() : void
+    protected $str1 = "A string, for example the title of a spam, or the text of an email, or even the content of a "
+            . "webpage... Who knows? With some additional characters to make it long enough... and some more "
+            . "characters, I hope I will evnetually reach a sufficient lenght....\n";
+
+    protected function setUp(): void
     {
-        $this->object = new SpamSum;
+        $this->object = new SpamSum();
     }
 
-
     /**
-     * @covers webd\language\SpamSum::Hash
-     * @todo   Implement testHash().
+     * @covers webd\language\SpamSum::hashString
+     * @todo   Implement testHashString().
      */
     public function testHashString()
     {
         $this->assertEquals(
-            $this->object->HashString($this->str1),
-            "6:MZEYWZDrpCGgFLLELGrX+TPdLgN98M6S8HROQ9Svb:M+hpTGgiNiM58LSj"
+            $this->object->hashString($this->str1),
+            "6:MZEYWZDrpCGgFLLELGrX+TPdLgN98M6S8HROQ9Svb:M+hpTGgiNiM58LSj",
         );
     }
 
     /**
-     * @covers webd\language\SpamSum::SetHashLength
+     * @covers webd\language\SpamSum::setHashLength
      * @todo   Implement testSetHashLength().
      */
     public function testSetHashLength()
     {
-        $s = new SpamSum;
-        $s->SetHashLength(10);
-        $s->HashString($this->str1);
-        $this->assertEquals(
-            "M0Gj58Lo",
-            $s->Left()
-        );
+        $s = new SpamSum();
+        $s->setHashLength(10);
+        $s->hashString($this->str1);
+        $this->assertEquals("M0Gj58Lo", $s->left());
     }
 
     /**
-     * @covers webd\language\SpamSum::SetLetters
+     * @covers webd\language\SpamSum::setLetters
      * @todo   Implement testSetLetters().
      */
     public function testSetLetters()
     {
-        $s = new SpamSum;
-        $s->SetLetters(8);
-        $s->HashString($this->str1);
+        $s = new SpamSum();
+        $s->setLetters(8);
+        $s->hashString($this->str1);
         $this->assertEquals(
             "EBEAGBDDBCGAFDDEDGDHGDHFDAFFEECCEHBGAFCHD",
-            $s->Left()
+            $s->left(),
         );
     }
-    
+
     public function testSetMinBlocksize()
     {
-        $s = new SpamSum;
-        $s->SetMinBlocksize(1);
-        $s->HashString($this->str1);
+        $s = new SpamSum();
+        $s->setMinBlocksize(1);
+        $s->hashString($this->str1);
         $this->assertEquals(
             "4:M1yuN7qZF30RqjKgBDlWdH0eKyXCBMqGUAiDmNA1XEGAnFNuoILPaFAAhNj:MLN7qZvjKgJU0VmC7GmSFL8PaFAAhh",
-            $s->__toString()
+            $s->__toString(),
         );
     }
 
 
     /**
-     * @covers webd\language\SpamSum::__toString
-     * @todo   Implement test__toString().
-     */
-    public function test__toString()
-    {
-    }
-
-    /**
-     * @covers webd\language\SpamSum::BlockSize
+     * @covers webd\language\SpamSum::blockSize
      * @todo   Implement testBlockSize().
      */
     public function testBlockSize()
@@ -95,7 +80,7 @@ class SpamSumTest extends TestCase
     }
 
     /**
-     * @covers webd\language\SpamSum::Left
+     * @covers webd\language\SpamSum::left
      * @todo   Implement testLeft().
      */
     public function testLeft()
@@ -103,7 +88,7 @@ class SpamSumTest extends TestCase
     }
 
     /**
-     * @covers webd\language\SpamSum::Right
+     * @covers webd\language\SpamSum::right
      * @todo   Implement testRight().
      */
     public function testRight()
